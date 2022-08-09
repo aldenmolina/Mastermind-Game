d484eb300697687c9a18cbab3912cc577ae2c698
Alden Molina <ajmolina092@gmail.com>
2022-08-09 15:28:59 -0400

    feat(add-first-attempt-functionality) - hint and win validation

---
+++ Mastermind.Console.App/Commands/Commands.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Mastermind.Console.App.Commands
+{
+    public class Commands
+    {
+        public static char[] CreateRandomAnswer()
+        {
+            var chars = "123456";
+            var answer = new char[4];
+            var random = new Random();
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = chars[random.Next(chars.Length)];
+            }
+            return answer;
+        }
+
+        public static bool CheckAnswer(char[] inputArray, char[] answer) =>
+            Enumerable.SequenceEqual(inputArray, answer);
+    }
+}
